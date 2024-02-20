4b613c135eb6e1bbfe24804d851101dc82c95fe5
jesdone <jesdone@gmail.com>
2024-02-20 13:03:43 -0500

    Built out App functionality

---
+++ src/components/Todo.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Todo = ({text, todo, todos, setTodos}) => {
+    //events
+    const deleteHandler = () => {
+        setTodos(todos.filter((el) => el.id !== todo.id))
+    }
+    const completeHandler = () => {
+        setTodos(todos.map(item => {
+            if(item.id === todo.id){
+                return {
+                    ...item, completed: !item.completed
+                }
+            }
+                return item;
+        })
+        );
+    };
+    // const editHandler = ({text, todo, todos, setTodos}) => {
+        // setTodos(todos.map(item => {
+        //     if(item.id === todo.id){
+        //         return {
+                    // ...item, text: !item.text
+            //     }
+            // }
+    //             return item;
+    //     })
+    //     );
+    // }
+
+  return (
+    <div className='todo'>
+        <li className={`todo-item ${todo.completed ? "completed" : ''}`}>{text}</li>
+        <button onClick={completeHandler} className='complete-btn'><i className='fas fa-check'>
+            </i></button>
+        <button onClick={deleteHandler} className='trash-btn'><i className='fas fa-trash'>
+            </i></button>
+        {/* <button onClick={editHandler} className='edit-btn'><i className='fas fa-edit'>
+            </i></button> */}
+    </div>
+  )
+}
+
+export default Todo;
